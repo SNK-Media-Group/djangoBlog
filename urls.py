06dd55ef3e412194c4752c98f98a28cde58e4514
Michael <59299019+csnewb@users.noreply.github.com>
2023-03-12 13:23:48 -0700

    project working now

--- urls.py
+++ urls.py
@@ -18,14 +18,30 @@ from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
 
-from posts.views import index, blog, post, search
+from posts.views import index, by_topic, post_by_slug, post_by_id, search, top_pick, newest
+from posts.views import terms_and_conditions, privacy_policy, do_not_sell
+from api.views import api
 
 urlpatterns = [
+    path('', newest),
+    path('newest/', newest, name='newest'),
+    path('top-picks/', top_pick, name='top-picks'),
+
+    path('search/', search, name='search'),
+
+    path('topic/<topic>', by_topic, name='post-list-by-topic'),
+
+    path('post/<slug>', post_by_slug, name='post-detail'),
+    path('post/id/<id>', post_by_id, name='post-detail-id'),
+
     path('admin/', admin.site.urls),
-    path('', index),
-    path('blog/', blog, name='post-list'),
-    path('post/<id>', post, name='post-detail'),
-    path('search/', search, name='search')
+    path('secret/api/', api, name='api'),
+
+    path('legal/terms-and-conditions', terms_and_conditions, name='terms-and-conditions'),
+    path('legal/privacy-policy', privacy_policy, name='privacy-policy'),
+    path('legal/do-not-sell', do_not_sell, name='do-not-sell'),
+
+
 ]
 
 

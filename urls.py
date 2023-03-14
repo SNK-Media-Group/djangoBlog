b7333a33ebe44a8b142104b211c1486533336d22
Michael Wieher <michael.wieher@gmail.com>
2023-03-14 15:23:12 -0400

    python -m black *.py

--- urls.py
+++ urls.py
@@ -21,15 +21,14 @@ from django.conf.urls.static import static
 from posts.views import index, blog, post, search
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', index),
-    path('blog/', blog, name='post-list'),
-    path('post/<id>', post, name='post-detail'),
-    path('search/', search, name='search')
+    path("admin/", admin.site.urls),
+    path("", index),
+    path("blog/", blog, name="post-list"),
+    path("post/<id>", post, name="post-detail"),
+    path("search/", search, name="search"),
 ]
 
 
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
